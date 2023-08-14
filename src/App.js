2163c282730c9e61b2056141e7ce26ab871a04ca
DilshodImomov <imomovdilshod2004@gmail.com>
2023-08-14 20:50:50 +0500

    Finishing the project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Result from './Containers/Result/Result';
+import Summary from './Containers/Summary/Summary';
 
 function App() {
   return (
-    <div className="container">
-      <Result />
+    <div className="block">
+      <div className="container">
+        <Result />
+        <Summary />
+      </div>
     </div>
   );
 }
